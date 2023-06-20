1e7751618dc59a9cab7a1fa9004736d0e570ed7a
KalyanDath <kalyandath18@gmail.com>
2023-06-20 14:42:38 +0530

    First commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.mainwindow import MainWindow
-
+    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
